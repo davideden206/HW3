2a5ff0741cf9c84393cca678b271f791a2b06c85
davideden206 <46527070+davideden206@users.noreply.github.com>
2019-01-18 11:04:41 +0200

    a

--- src/BCM.java
+++ src/BCM.java
@@ -39,91 +39,41 @@ public class BCM  {
  			id=ADMIN , name =ADMIN
 		 */
 
-		OpenSer.openFile();
-		sch = OpenSer.readRecords();
-		OpenSer.closeFile();
-		saveSchedule();
+		// open and lobe ser file
+				OpenSer.openFile();
+				sch = OpenSer.readRecords();
+				OpenSer.closeFile();
+				
+				saveSchedule();
+				
 
-		System.out.println(sch);
+				System.out.println(sch);
+				System.out.println("<<<<<<<< the managers >>>>>>>>");
+				System.out.println(managers);
 
-		// end mane	
+				
+				// ran at the first frame
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
 
-
-
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-
-					LogINFrame window = new LogINFrame();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+								LogINFrame window = new LogINFrame();
+								window.frame.setVisible(true);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
 			}
-		});
-	}
-
-	// Mutates for add program
-
-	public static void saveSchedule() {
-		FileOut.openFileSer();
-		FileOut.addRecordsSer(sch.ScheduletoArry());
-		FileOut.closeFileSer();
-	}
-
-
-	public static void addNews(Schedule sch)
-	{
-		for (News n: Filein.news)
-		{
-			try {
-				Filein.manager.get(0).addProgramByManger(n,sch);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			
+			// Mutates for add program
+			/**
+			 * save the Schedole in Ser file
+			 */
+			public static void saveSchedule() {
+			FileOut.openFileSer();
+			FileOut.addRecordsSer(sch.ScheduletoArry());
+			FileOut.closeFileSer();
 			}
+			
 		}
-	}
-	public static void addMove(Schedule sch)
-	{
-		for (Movie m : Filein.movie)
-		{
-			try {
-				Filein.manager.get(1).addProgramByManger(m, sch);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	public static void addSerise(Schedule sch)
-	{
-		for(Series s : Filein.series)
-		{
-			try {
-				Filein.manager.get(2).addProgramByManger(s, sch);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	public static void addTvshow(Schedule sch)
-	{
-		for (TVShow t : Filein.tvShow)
-		{
-			try {
-				Filein.manager.get(3).addProgramByManger(t, sch);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
-
-
-
-
-
-}
